8329a8c9d52acd220a95cfbe2c20f824b3b3460c
sagar <sagargurung272@gmail.com>
2025-01-31 11:09:18 +0530

    Added keys

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,19 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
 # Database URL from config
-DATABASE_URL = settings.DATABASE_URL
+#DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:sagar@localhost/dbname")
 
 # SQLAlchemy setup
+
+#Create the database engine
 engine = create_engine(DATABASE_URL)
+
+#Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Define the base class for models
 Base = declarative_base()
 
 def get_db():
     """
-    Dependency to provide a database session.
+    Provides a database session.
     """
     db = SessionLocal()
     try:
